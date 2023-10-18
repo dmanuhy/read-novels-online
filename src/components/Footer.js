2e250cfa92d86125e717ae23deddc077278b9b00
Manh <manuhy04@gmail.com>
2023-10-18 20:40:28 +0700

    navbar with react-bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,13 @@ import "../assets/css/footer.scss"
 const Footer = () => {
     return (
         <>
-            <footer class="footer">
-                <div class="container">
-                    <div class="row d-flex justify-content-between text-white">
-                        <div class="col-6 p-5 text-center">
+            <footer className="footer">
+                <div className="container">
+                    <div className="row d-flex justify-content-between text-white">
+                        <div className="col-6 p-5 text-center">
                             <div>@Copyright 2023</div>
                         </div>
-                        <div class="col-6 p-5 text-center">
+                        <div className="col-6 p-5 text-center">
                             <div>
                                 <span>More Information</span>
                             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,87 @@
 import logo from "../assets/image/logo.webp"
 import "../assets/css/header.scss"
 import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 const Header = () => {
     return (
         <>
             <div className="home-header">
-                <nav className="navbar navbar-expand-lg p-0">
+                <Navbar expand="lg">
+                    <Container>
+                        <Navbar.Brand href="/">
+                            <img className="navbar-brand" src={logo} alt="Logo" />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-white" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <div className="navbar-nav d-flex flex-lg-row flex-column justify-content-between">
+                                <div className="left-menu d-flex flex-lg-row flex-column align-items-lg-center mb-lg-0 mb-2">
+                                    <Dropdown className="dropdown text-white genre">
+                                        <Dropdown.Toggle className="dropdown-hover">
+                                            <span className="">Thể Loại</span>
+                                        </Dropdown.Toggle>
+
+                                        <Dropdown.Menu className="dropdown-menu">
+                                            <div className="genre-list row px-2">
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                            </div>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+
+                                    <Dropdown className="dropdown text-white genre">
+                                        <Dropdown.Toggle className="dropdown-hover">
+                                            <span className="">Tùy Chỉnh</span>
+                                        </Dropdown.Toggle>
+
+                                        <Dropdown.Menu >
+                                            <div className="genre-list row px-2">
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                            </div>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </div>
+                                <div className="right-menu d-flex flex-lg-row align-items-lg-center flex-column mb-lg-0 mb-2">
+                                    <input type="text" className="mx-2 ps-3 border rounded-1 search my-1"
+                                        placeholder="Tìm truyện ..." />
+                                    <div className="login-btn">
+                                        <a href="/" className="text-decoration-none text-white dropdown-hover">Đăng nhập</a>
+                                    </div>
+                                    <div className="register-btn">
+                                        <a href="/" className="text-decoration-none text-white dropdown-hover">Đăng ký</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                {/* <nav className="navbar navbar-expand-lg p-0">
                     <div className="container">
                         <img className="navbar-brand" src={logo} alt="Logo" />
                         <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse"
@@ -23,38 +98,21 @@ const Header = () => {
                                         </Dropdown.Toggle>
 
                                         <Dropdown.Menu className="dropdown-menu">
-                                            <div className="row">
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Bách Hợp</a></li>
-                                                        <li><a className="" href="#">Cổ Đại</a></li>
-                                                        <li><a className="" href="#">Cung Đấu</a></li>
-                                                        <li><a className="" href="#">Dị Năng</a></li>
-                                                    </ul>
+                                            <div className="genre-list row px-2">
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Đoản Văn</a></li>
-                                                        <li><a className="" href="#">Đam Mỹ</a></li>
-                                                        <li><a className="" href="#">Huyền Huyễn</a></li>
-                                                        <li><a className="" href="#">Kiếm Hiệp</a></li>
-                                                    </ul>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Lịch Sử</a></li>
-                                                        <li><a className="" href="#">Light Novel</a></li>
-                                                        <li><a className="" href="#">Mạt Thế</a></li>
-                                                        <li><a className="" href="#">Hài Hước</a></li>
-                                                    </ul>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Nữ Cường</a></li>
-                                                        <li><a className="" href="#">Ngược</a></li>
-                                                        <li><a className="" href="#">Truyện Teen</a></li>
-                                                        <li><a className="" href="#">Trọng Sinh</a></li>
-                                                    </ul>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
                                             </div>
                                         </Dropdown.Menu>
@@ -66,38 +124,21 @@ const Header = () => {
                                         </Dropdown.Toggle>
 
                                         <Dropdown.Menu >
-                                            <div className="row">
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Bách Hợp</a></li>
-                                                        <li><a className="" href="#">Cổ Đại</a></li>
-                                                        <li><a className="" href="#">Cung Đấu</a></li>
-                                                        <li><a className="" href="#">Dị Năng</a></li>
-                                                    </ul>
+                                            <div className="genre-list row px-2">
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Đoản Văn</a></li>
-                                                        <li><a className="" href="#">Đam Mỹ</a></li>
-                                                        <li><a className="" href="#">Huyền Huyễn</a></li>
-                                                        <li><a className="" href="#">Kiếm Hiệp</a></li>
-                                                    </ul>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Lịch Sử</a></li>
-                                                        <li><a className="" href="#">Light Novel</a></li>
-                                                        <li><a className="" href="#">Mạt Thế</a></li>
-                                                        <li><a className="" href="#">Hài Hước</a></li>
-                                                    </ul>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
-                                                <div className="col-3">
-                                                    <ul className="ul-menu">
-                                                        <li><a className="" href="#">Nữ Cường</a></li>
-                                                        <li><a className="" href="#">Ngược</a></li>
-                                                        <li><a className="" href="#">Truyện Teen</a></li>
-                                                        <li><a className="" href="#">Trọng Sinh</a></li>
-                                                    </ul>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
+                                                </div>
+                                                <div className="genre-item col-3">
+                                                    <a className="text-decoration-none text-white" href="/">Bách Hợp</a>
                                                 </div>
                                             </div>
                                         </Dropdown.Menu>
@@ -107,16 +148,16 @@ const Header = () => {
                                     <input type="text" className="mx-1 mb-lg-0 mb-2 ps-3 border rounded-1 search"
                                         placeholder="Tìm truyện ..." />
                                     <div className="login-btn my-lg-0 my-2">
-                                        <a className="text-decoration-none text-white dropdown-hover">Đăng nhập</a>
+                                        <a href="/" className="text-decoration-none text-white dropdown-hover">Đăng nhập</a>
                                     </div>
                                     <div className="logout-btn my-lg-0 my-2">
-                                        <a className="text-decoration-none text-white dropdown-hover">Đăng ký</a>
+                                        <a href="/" className="text-decoration-none text-white dropdown-hover">Đăng ký</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </nav >
+                </nav > */}
             </div >
 
             <div className="header-introduction container">
